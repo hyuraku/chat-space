ccb2e87de94e5fe1d945a842dd6c35ec3c5ee928
hyuraku <yu@hill-wood.com>
2018-03-29 20:47:30 +0900

    インクリメントサーチの実装
    ユーザのnameにindexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313112243) do
+ActiveRecord::Schema.define(version: 20180327133950) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20180313112243) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
