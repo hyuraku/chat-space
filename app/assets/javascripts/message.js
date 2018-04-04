4897543acfd2de2ba653a34f5e91b248baa7f2c3
hyuraku <yu@hill-wood.com>
2018-04-04 20:24:12 +0900

    自動更新機能の追加
    jsファイルの修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function() {
   function buildHTML(message) {
-    var html = `<div class="chatMain__body__oneMess">
+    var html = `<div class="chatMain__body__oneMess" data-message-id=${message.id}>
                 ${message.user_name}
                 <div class="chatMain__body__oneMess__time">
                 ${message.created_at}
@@ -8,9 +8,9 @@ $(function() {
                 </div>
                 <p class="chatMain__body__detail">
                 ${message.text}
-                </p>`
+                </p>`;
     if (message.image.url) {
-      html = html + `<img class="chatMain__body__image" src=${message.image.url} alt="" >`
+      html = html + `<img class="chatMain__body__image" src=${message.image.url} alt="" >`;
     }
 
     return html;
@@ -34,12 +34,39 @@ $(function() {
         var html = buildHTML(data);
         $('.chatMain__body').append(html);
         $('.chatMain__footer__body').val('');
-        $('.chatMain__footer__body--hidden').val('');  
+        $('.chatMain__footer__body--hidden').val('');
       })
       .fail(function() {
         alert('error');
-      })
+      });
     $('.chatMain__body').animate({
-      scrollTop: $('.chatMain__body')[0].scrollHeight }, 'fast')
-  })
+      scrollTop: $('.chatMain__body')[0].scrollHeight
+    }, 'fast');
+  });
+
+  var interval = setInterval(function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      $.ajax({
+          url: location.href.json,
+          type:'GET',
+          dataType:'json',
+        })
+        .done(function(json) {
+          var insertHTML = '';
+          var present_mess = $('.chatMain__body__oneMess')
+          var id = present_mess[present_mess.length - 1].getAttribute("data-message-id");
+          json.messages.forEach(function(message) {
+            if (message.id > id) {
+              insertHTML += buildHTML(message);
+            }
+          });
+          $('.chatMain__body').prepend(insertHTML);
+        })
+        .fail(function(json) {
+          alert("自動更新に失敗しました");
+        });
+    } else {
+      clearInterval(interval);
+    }
+  }, 5 * 1000);
 });
