194c375b745dac2c22b3236be49fff3a4efb8a76
hyuraku <yu@hill-wood.com>
2018-04-07 14:40:56 +0900

    自動更新機能の実装
    一部修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user).where("id > ?" , "%#{params[:keyword]}%")
+    @messages = @group.messages.includes(:user).where("id > ?" , "#{params[:id]}")
     respond_to do |format|
       format.html
       format.json
